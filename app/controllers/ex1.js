0d5afa0e286e9b3aea4bc76428e57e270951e60c
Levallois Lucas <21901281@et.unicaen.fr>
2021-01-26 16:19:36 +0100

    ex1. js et ex1.hbs

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -1,4 +1,46 @@
 import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class Ex1Controller extends Controller {
+  @tracked content='Default';
+  @tracked info = '';
+
+  messageNote = '';
+  @tracked styles = 'alert-info';
+  const;
+  MAX=100;
+
+  get size() {
+    return this.MAX - this.content.length;
+  }
+
+get style(){
+  if(this.content.length > 0){
+    return 'alert alert-info';
+  }else{
+    return '';
+  }
+}
+
+@action
+update(){
+  if(this.content.length > 0){
+    this.info = 'Note modifiée';
+  }else{
+    this.info = '';
+  }
+}
+
+@action
+save(){
+  if(this.content.length > 0){
+    this.info = 'Note sauvegardée';
+    this.style = 'alert-success';
+    this.messageNote = this.content.toString();
+  }else{
+    this.info = '';
+  }
+}
+
 }
