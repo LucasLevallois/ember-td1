3e1c92e1ab7ce1cc91bae69d0a33a992f4a236c5
Levallois Lucas <21901281@et.unicaen.fr>
2021-03-18 00:42:05 +0100

    TD6

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,5 @@
-import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import RESTAdapter from '@ember-data/adapter/rest';
 
-export default class ApplicationAdapter extends JSONAPIAdapter {
+export default class ApplicationAdapter extends RESTAdapter {
+  host = 'http://127.0.0.1:8091/rest';
 }
